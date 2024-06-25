dd2b0940656bde4ffa4bc9318ec8936844b582e1
abigailnadua <acnadua@up.edu.ph>
2024-06-25 19:21:26 +0800

    added template

--- main.dart
+++ main.dart
@@ -1,16 +1,7 @@
-bool isAnagram(String string1, String string2) {}
-
-void displayCharacterCount(String string1, String string2) {}
-
 void main() {
   // sample test case
   String string1 = "le sserafim";
   String string2 = "i'm fearless";
 
-  if (isAnagram(string1, string2)) {
-    print('"$string1" and "$string2" are anagrams');
-    displayCharacterCount(string1, string2);
-  } else {
-    print('"$string1" and "$string2" are not anagrams');
-  }
+  anagramChecker(string1, string2);
 }

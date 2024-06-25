82d76c4921556f84e6ac5b17a0c445dcdd8c165d
abigailnadua <acnadua@up.edu.ph>
2024-06-25 19:15:55 +0800

    added code to the template

--- main.dart
+++ main.dart
@@ -1,9 +1,16 @@
-void isAnagram(String string1, String string2) {}
+bool isAnagram(String string1, String string2) {}
+
+void displayCharacterCount(String string1, String string2) {}
 
 void main() {
   // sample test case
   String string1 = "le sserafim";
   String string2 = "i'm fearless";
 
-  isAnagram(string1, string2);
+  if (isAnagram(string1, string2)) {
+    print('"$string1" and "$string2" are anagrams');
+    displayCharacterCount(string1, string2);
+  } else {
+    print('"$string1" and "$string2" are not anagrams');
+  }
 }
